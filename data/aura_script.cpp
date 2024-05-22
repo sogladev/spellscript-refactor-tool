999882e9389771173193080652e861cda32d143e
Jelle Meeus <sogladev@gmail.com>
2024-05-22 14:51:00 +0200

    add script extracts

---
+++ data/aura_script.cpp
@@ -0,0 +1,34 @@
+
+// 63830, 63881 - Malady of the Mind
+class spell_yogg_saron_malady_of_the_mind : public SpellScriptLoader
+{
+public:
+    spell_yogg_saron_malady_of_the_mind() : SpellScriptLoader("spell_yogg_saron_malady_of_the_mind") { }
+
+    class spell_yogg_saron_malady_of_the_mind_AuraScript : public AuraScript
+    {
+        PrepareAuraScript(spell_yogg_saron_malady_of_the_mind_AuraScript);
+
+        void OnApply(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
+        {
+            GetUnitOwner()->ApplySpellImmune(SPELL_DEATH_RAY_DAMAGE_REAL, IMMUNITY_ID, SPELL_DEATH_RAY_DAMAGE_REAL, true);
+        }
+
+        void OnRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
+        {
+            GetUnitOwner()->ApplySpellImmune(SPELL_DEATH_RAY_DAMAGE_REAL, IMMUNITY_ID, SPELL_DEATH_RAY_DAMAGE_REAL, false);
+            GetUnitOwner()->CastCustomSpell(SPELL_MALADY_OF_THE_MIND_TRIGGER, SPELLVALUE_MAX_TARGETS, 1, GetUnitOwner(), true);
+        }
+
+        void Register() override
+        {
+            OnEffectApply += AuraEffectApplyFn(spell_yogg_saron_malady_of_the_mind_AuraScript::OnApply, EFFECT_1, SPELL_AURA_MOD_FEAR, AURA_EFFECT_HANDLE_REAL);
+            OnEffectRemove += AuraEffectRemoveFn(spell_yogg_saron_malady_of_the_mind_AuraScript::OnRemove, EFFECT_1, SPELL_AURA_MOD_FEAR, AURA_EFFECT_HANDLE_REAL);
+        }
+    };
+
+    AuraScript* GetAuraScript() const override
+    {
+        return new spell_yogg_saron_malady_of_the_mind_AuraScript();
+    }
+};
---
+++ data/spell_script.cpp
@@ -0,0 +1,45 @@
+
+// 64172 - Titanic Storm
+class spell_yogg_saron_titanic_storm : public SpellScriptLoader
+{
+public:
+    spell_yogg_saron_titanic_storm() : SpellScriptLoader("spell_yogg_saron_titanic_storm") { }
+
+    class spell_yogg_saron_titanic_storm_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_yogg_saron_titanic_storm_SpellScript);
+
+        void HandleDummyEffect(SpellEffIndex effIndex)
+        {
+            PreventHitDefaultEffect(effIndex);
+            if (Unit* target = GetHitUnit())
+                Unit::Kill(GetCaster(), target);
+        }
+
+        void FilterTargets(std::list<WorldObject*>& targets)
+        {
+            WorldObject* target = nullptr;
+            for (std::list<WorldObject*>::iterator itr = targets.begin(); itr != targets.end(); ++itr)
+                if ((*itr)->ToUnit()->HasAura(SPELL_WEAKENED))
+                {
+                    target = *itr;
+                    break;
+                }
+
+            targets.clear();
+            if (target)
+                targets.push_back(target);
+        }
+
+        void Register() override
+        {
+            OnEffectHitTarget += SpellEffectFn(spell_yogg_saron_titanic_storm_SpellScript::HandleDummyEffect, EFFECT_0, SPELL_EFFECT_DUMMY);
+            OnObjectAreaTargetSelect += SpellObjectAreaTargetSelectFn(spell_yogg_saron_titanic_storm_SpellScript::FilterTargets, EFFECT_0, TARGET_UNIT_SRC_AREA_ENTRY);
+        }
+    };
+
+    SpellScript* GetSpellScript() const override
+    {
+        return new spell_yogg_saron_titanic_storm_SpellScript();
+    }
+};
