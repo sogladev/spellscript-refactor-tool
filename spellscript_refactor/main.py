ede8133b242c9eec73eaf4b2f18b28ca5f1a041c
Jelle Meeus <sogladev@gmail.com>
2024-05-22 20:38:45 +0200

    add skip

--- spellscript_refactor/main.py
+++ spellscript_refactor/main.py
@@ -2,15 +2,14 @@ import argparse
 
 from .refactor import format_first_block_in_file
 from .util.logger import logger
-from .util.colors import color, Color
 
 def main():
-    logger.info("Program started")
     parser = argparse.ArgumentParser()
     parser.add_argument('source_file', type=str)
     parser.add_argument('dest_file', type=str)
+    parser.add_argument('--skip', type=int, default=0, help="amount of lines to skip")
     args = parser.parse_args()
-    format_first_block_in_file(args.source_file, args.dest_file)
+    format_first_block_in_file(args.source_file, args.dest_file, skip=args.skip)
 
 if __name__ == '__main__':
     main()
