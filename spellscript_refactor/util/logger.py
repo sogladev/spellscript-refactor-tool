ecfd7638d831a07fd09a2d13ff4d5b5b9d4d1dc7
Jelle Meeus <sogladev@gmail.com>
2024-06-09 21:39:35 +0200

    add file log only if debugging

--- spellscript_refactor/util/logger.py
+++ spellscript_refactor/util/logger.py
@@ -2,7 +2,7 @@ import logging
 
 # Create a logger
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 # Create a formatter to define the log format
 format = "%(levelname)s:%(filename)s:%(lineno)03d:%(funcName)20s():%(message)s"
@@ -10,15 +10,16 @@ format = "%(levelname)s:%(filename)s:%(lineno)03d:%(funcName)20s():%(message)s"
 formatter = logging.Formatter(format)
 
 # Create a file handler to write logs to a file
-file_handler = logging.FileHandler('logs/app.log')
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
+if logger.level != logging.INFO:
+    file_handler = logging.FileHandler('logs/app.log')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
 # Create a stream handler to print logs to the console
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)  # You can set the desired log level for console output
 console_handler.setFormatter(formatter)
 
-# Add the handlers to the logger
-logger.addHandler(file_handler)
+# Add the handler to the logger
 logger.addHandler(console_handler)
