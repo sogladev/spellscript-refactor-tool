2addf9f208264b44e96a8c22a244786d89aaf8bd
Jelle Meeus <sogladev@gmail.com>
2024-06-17 01:26:07 +0200

    add init tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,10 +1,25 @@
 import pytest
 
+import mysql.connector
+
 from acore_spellscript_refactor.util.db import *
 
 def test_always_passes():
     assert True
 
-def test_db_lookup_ids():
-    result = db_lookup_ids('test')
+def test_db_lookup_ids_empty():
+    result = db_lookup_ids('this_spell_does_not_exist_12379832')
     assert len(result) == 0
+
+def test_failed_connection():
+    try:
+        mysql.connector.connect(
+            host="localhost",
+            user="acore",
+            password="wrongpassword",
+            database="acore_world"
+        )
+    except mysql.connector.Error:
+        assert True
+        return
+    assert False
